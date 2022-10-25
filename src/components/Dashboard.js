6133cd1b53d5c32a1d53e5bab3c77bdc1fd97fa0
Antonio Ribeiro <antonio@genialcare.com.br>
2022-10-25 16:05:37 -0300

    Adds tabview to dashboard page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -6,25 +6,62 @@ const Dashboard = (props) => {
   const answeredPolls = props.answeredPolls;
 
   return (
-    <div className="container mt-5">
-      <div className="card m-4">
-        <div className="card-header">
-          New Polls: {newPolls.length}
+    <div className="container mt-4">
+      <ul className="nav nav-tabs" id="pollsTab" role="tablist">
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link active"
+            id="home-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#home-tab-pane"
+            type="button" role="tab"
+            aria-controls="home-tab-pane"
+            aria-selected="true">
+              New Polls: {newPolls.length}
+            </button>
+        </li>
+        <li className="nav-item" role="presentation">
+          <button
+            className="nav-link"
+            id="profile-tab"
+            data-bs-toggle="tab"
+            data-bs-target="#profile-tab-pane"
+            type="button"
+            role="tab"
+            aria-controls="profile-tab-pane"
+            aria-selected="false">
+              Answered Polls: {answeredPolls.length}
+          </button>
+        </li>
+      </ul>
+      <div className="tab-content" id="myTabContent">
+        <div
+          className="tab-pane fade show active"
+          id="home-tab-pane"
+          role="tabpanel"
+          aria-labelledby="home-tab"
+          tabIndex="0">
+          <div className="card">
+            <div className="card-body">
+              {newPolls.map((id) => (
+                <PollCard key={id} id={id} />
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="card-body">
-          {newPolls.map((id) => (
-            <PollCard key={id} id={id} />
-          ))}
-        </div>
-      </div>
-      <div className="card m-4">
-        <div className="card-header">
-          Answered Polls: {answeredPolls.length}
-        </div>
-        <div className="card-body">
-          {answeredPolls.map((id) => (
-            <PollCard key={id} id={id} />
-          ))}
+        <div
+          className="tab-pane fade"
+          id="profile-tab-pane"
+          role="tabpanel"
+          aria-labelledby="profile-tab"
+          tabIndex="0">
+          <div className="card">
+            <div className="card-body">
+              {answeredPolls.map((id) => (
+                <PollCard key={id} id={id} />
+              ))}
+            </div>
+          </div>
         </div>
       </div>
     </div>
